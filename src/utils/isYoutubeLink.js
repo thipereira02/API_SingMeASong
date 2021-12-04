d121484c954bce36cb5b7fbbbd238e6806a87888
thipereira02 <thiago.spereira02@gmail.com>
2021-12-04 19:57:27 -0300

    Revert "Implementing unit tests to recommendationService"
    This reverts commit d25f7cfcffcef0d1d3912719dab33207778b4e72.

--- src/utils/isYoutubeLink.js
+++ src/utils/isYoutubeLink.js
@@ -1,6 +1,6 @@
 function isYoutubeVideo(youtubeLink) {
-  const isValid = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
-  return !!(youtubeLink.match(isValid));
+  const v = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+  return !!(youtubeLink.match(v));
 }
 
 export { isYoutubeVideo };
